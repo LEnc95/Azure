07d9465f7280d5a4d2ce12d70b30a52e7f334294
Luke Encrapera <luke.encrapera@gianteagle.com>
2022-08-29 10:37:01 -0400

    August Updates

--- Azure-DynamicGroupMembershipRuleExplicitAdditions.ps1
+++ Azure-DynamicGroupMembershipRuleExplicitAdditions.ps1
@@ -30,7 +30,7 @@ $AzureHeaders          = @{'Authorization'="$($oAuth.token_type) $($oAuth.access
 #endregion Authentication
 #>
 
-$targetedGroups = (Get-ADGroup -filter { DisplayName -like "*_GetGoStoreLeadership" } -SearchBase "OU=O365,OU=Exchange,DC=corp,DC=gianteagle,DC=com") 
+$targetedGroups = (Get-ADGroup -filter { DisplayName -like "*_CurbsideExpress" } -SearchBase "OU=O365,OU=Exchange,DC=corp,DC=gianteagle,DC=com") 
 $DynamicGroupSet = foreach ($group in $targetedGroups) { Get-AzureADMSGroup -Id $group.Name.Split("_")[1] }
 $DynamicGroupSet | Export-Csv -Path C:\Temp\DynamicGroupSet.csv -Force -NoTypeInformation
 foreach ($group in $DynamicGroupSet) { $storeInfo.add($group.DisplayName.Split("_")[0], $group.Id) }
@@ -64,14 +64,15 @@ $reportedAdds[0] | Out-File -FilePath C:\Temp\reportAdds.txt
 
 $reportHash.Keys | foreach-object {
     $adds += " " + $reportHash["$_"]
-    $filterRule = '(user.Department -contains ' + '"' + $_ + '")' + '-and (user.extensionAttribute3 -eq "A") -and ((user.extensionAttribute13 -In ["10041","10046","10060","10061","10062","10080","10084","10085","10086","10087","10105","10140","10141","10143","10144","10145","10168","10169","10174","10176","10177","10178","10182","21083","80047","80123","80207","80215","80225","80232"]) -or (user.extensionAttribute6 -contains "10041") -or (user.extensionAttribute6 -contains "10046") -or (user.extensionAttribute6 -contains "10060") -or (user.extensionAttribute6 -contains "10061") -or (user.extensionAttribute6 -contains "10062") -or (user.extensionAttribute6 -contains "10080") -or (user.extensionAttribute6 -contains "10084") -or (user.extensionAttribute6 -contains "10085") -or (user.extensionAttribute6 -contains "10086") -or (user.extensionAttribute6 -contains "10087") -or (user.extensionAttribute6 -contains "10105") -or (user.extensionAttribute6 -contains "10140") -or (user.extensionAttribute6 -contains "10141") -or (user.extensionAttribute6 -contains "10143") -or (user.extensionAttribute6 -contains "10144") -or (user.extensionAttribute6 -contains "10145") -or (user.extensionAttribute6 -contains "10168") -or (user.extensionAttribute6 -contains "10169") -or (user.extensionAttribute6 -contains "10174") -or (user.extensionAttribute6 -contains "10176") -or (user.extensionAttribute6 -contains "10177") -or (user.extensionAttribute6 -contains "10178") -or (user.extensionAttribute6 -contains "21083") -or (user.extensionAttribute6 -contains "80047") -or (user.extensionAttribute6 -contains "80123") -or (user.extensionAttribute6 -contains "80207") -or (user.extensionAttribute6 -contains "80215") -or (user.extensionAttribute6 -contains "80225"))' 
+    $filterRule = '(user.Department -contains ' + '"' + $_ + '") ' + '-and (user.extensionAttribute3 -eq "A") -and ((user.extensionAttribute13 -In ["10147","10148","10180","31310","80119","80144","80155","80189","10183","88052","80246"]) -or (user.extensionAttribute6 -contains "10147") -or (user.extensionAttribute6 -contains "10148") -or (user.extensionAttribute6 -contains "10180") -or (user.extensionAttribute6 -contains "31310") -or (user.extensionAttribute6 -contains "80119") -or (user.extensionAttribute6 -contains "80144") -or (user.extensionAttribute6 -contains "80155") -or (user.extensionAttribute6 -contains "80189") -or (user.extensionAttribute6 -contains "10183") -or (user.extensionAttribute6 -contains "88052"))' 
     #(user.Department -contains "3395") -and (user.extensionAttribute3 -eq "A") -and ((user.extensionAttribute13 -In ["10041","10046","10060","10061","10062","10080","10084","10085","10086","10087","10105","10140","10141","10143","10144","10145","10168","10169","10174","10176","10177","10178","21083","80047","80123","80207","80215","80225","10182","80232"]) -or (user.extensionAttribute6 -contains "10041") -or (user.extensionAttribute6 -contains "10046") -or (user.extensionAttribute6 -contains "10060") -or (user.extensionAttribute6 -contains "10061") -or (user.extensionAttribute6 -contains "10062") -or (user.extensionAttribute6 -contains "10080") -or (user.extensionAttribute6 -contains "10084") -or (user.extensionAttribute6 -contains "10085") -or (user.extensionAttribute6 -contains "10086") -or (user.extensionAttribute6 -contains "10087") -or (user.extensionAttribute6 -contains "10105") -or (user.extensionAttribute6 -contains "10140") -or (user.extensionAttribute6 -contains "10141") -or (user.extensionAttribute6 -contains "10143") -or (user.extensionAttribute6 -contains "10144") -or (user.extensionAttribute6 -contains "10145") -or (user.extensionAttribute6 -contains "10168") -or (user.extensionAttribute6 -contains "10169") -or (user.extensionAttribute6 -contains "10174") -or (user.extensionAttribute6 -contains "10176") -or (user.extensionAttribute6 -contains "10177") -or (user.extensionAttribute6 -contains "10178") -or (user.extensionAttribute6 -contains "21083") -or (user.extensionAttribute6 -contains "80047") -or (user.extensionAttribute6 -contains "80123") -or (user.extensionAttribute6 -contains "80207") -or (user.extensionAttribute6 -contains "80215") -or (user.extensionAttribute6 -contains "80225"))
     $out += $filterRule + $adds
     $out | Out-File -FilePath C:\Temp\whatIsSet.txt 
     $filterRule = $filterRule + $adds
-    #Set-AzureADMSGroup -id $storeinfo["$_"] -membershipRule $filterRule
-    #$storeinfo["$_"]
-    #$filterRule
+    Set-AzureADMSGroup -id $storeinfo["$_"] -membershipRule $filterRule
+    $storeinfo["$_"]
+    $filterRule
+
     <#
     [uri]$SignInsUrl = "https://graph.microsoft.com/v1.0/auditLogs/signIns?`$filter=userPrincipalName eq '$_'"
     $SignIns = Invoke-RestMethod -Uri $SignInsUrl.AbsoluteUri -Headers $AzureHeaders
